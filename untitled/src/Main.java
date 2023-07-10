73db9a2ffa9f55cc7004953058ffb059eeb1da7b
ronald sarmiento <ronisarmont@gmail.com>
2023-07-10 13:12:15 -0400

    cambiamdo la ventana y usando un JOptionPane

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,10 +4,8 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         //componente JFrame
-       JFrame miJFrame = new JFrame("Ejemplo de Frame- java Swing");
+       JFrame miJFrame = new JFrame("Menu");
        miJFrame.setSize(500,300);
-
-
 
        //componente JPanel();
         JPanel miJPanel = new JPanel();
@@ -18,12 +16,18 @@ public class Main {
         miJPanel.setLayout(new GridBagLayout());
         //componente JtextField
         JLabel miJLabel = new JLabel();
-        miJLabel.setText("Dime tu opinión acerca de Java Swing:  ");
-        //componente JtextArea
-        JTextArea miJTextArea = new JTextArea(5,20);
+        //miJLabel.setText("Menu");
+        //componente JOption
+        JOptionPane miJOptionPane = new JOptionPane();
+        //
+        String conversorOpciones = (JOptionPane.showInputDialog(null,"Seleccionar conversion",
+                "Menu",JOptionPane.PLAIN_MESSAGE,null,
+                new Object[]{"Conversor de Moneda","Conversor de Temperatura"},
+                "Selecciona")).toString();
 
-        miJPanel.add(miJLabel);
-        miJPanel.add(miJTextArea);
+
+        //miJPanel.add(miJLabel);
+        miJPanel.add(miJOptionPane);
 
         miJFrame.add(miJPanel);
         miJFrame.setVisible(true);
