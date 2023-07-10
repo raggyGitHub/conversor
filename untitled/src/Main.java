f200ed082f8e6e15023eeb8a56f90dbdd6205096
ronald sarmiento <ronisarmont@gmail.com>
2023-07-10 10:23:41 -0400

    nuevo main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //componente JFrame
+       JFrame miJFrame = new JFrame("Ejemplo de Frame- java Swing");
+       miJFrame.setSize(500,300);
+
+
+
+       //componente JPanel();
+        JPanel miJPanel = new JPanel();
+        miJPanel.setSize(300,300);
+
+        // usamos este diseño para centrar los componentes de JPanel`
+
+        miJPanel.setLayout(new GridBagLayout());
+        //componente JtextField
+        JLabel miJLabel = new JLabel();
+        miJLabel.setText("Dime tu opinión acerca de Java Swing:  ");
+        //componente JtextArea
+        JTextArea miJTextArea = new JTextArea(5,20);
+
+        miJPanel.add(miJLabel);
+        miJPanel.add(miJTextArea);
+
+        miJFrame.add(miJPanel);
+        miJFrame.setVisible(true);
     }
 }
